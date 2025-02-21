7662b41747dc9082f6ee1f8c50301db6623c962a
gusenga thierry <t.gusenga@alustudent.com>
2025-02-21 19:41:48 +0200

    Update 1-main.py

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 """
-Prints plz pass an argument.
+1-main
 """
-import sys
-from 1-top_ten import top_ten
+import requests
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+def top_ten(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'python:top_ten:v1.0 (by /u/wintermancer)'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+
+    if 'data' not in data:
+        print(None)
+        return
+
+    posts = data['data']['children']
+
+    for post in posts:
+        print(post['data']['title'])
