e614e60b4ad2769de48b4265a8cab05a0dcfb14f
gusenga thierry <t.gusenga@alustudent.com>
2025-02-21 19:34:44 +0200

    main

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,10 +1,31 @@
-#!/usr/bin/python3
-"""1-main"""
-import sys
-from 1-top_ten import top_ten
+import requests
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+def recurse(subreddit, hot_list=[], after=None):
+    """Recursively fetches hot post titles from a given subreddit."""
+    
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit) 
+    header = {'User-Agent': 'Mozilla/5.0'} 
+    param = {'after': after}
+    response = requests.get(url, headers=header, params=param, allow_redirects=False)
+    if response.status_code != 200:
+        return None
     else:
-        top_ten(sys.argv[1])
+        json_res = response.json()
+        after = json_res.get('data').get('after')
+        has_next = after is not None
+        hot_articles = json_res.get('data').get('children')
+        [hot_list.append(article.get('data').get('title')) for article in hot_articles]
+        return recurse(subreddit, hot_list, after=after) if has_next else hot_list
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    hot_list = recurse(subreddit)
+    if hot_list is None:
+        print(None)
+    else:
+        for title in hot_list:
+            print(title)
+        json_res = response.json()
+        hot_articles = json_res.get('data', {}).get('children', [])
+        for article in hot_articles:
+            print(article.get('data', {}).get('title', ''))
