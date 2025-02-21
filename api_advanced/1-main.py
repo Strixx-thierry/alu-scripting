380d79afa53234cefd86539c46e553f26c4cd093
Gusenga Thierry <t.gusenga@alustudent.com>
2025-02-21 20:18:32 +0200

    Update 1-main.py

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -2,25 +2,11 @@
 """
 1-main
 """
-import requests
+import sys
 
-def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'python:top_ten:v1.0 (by /u/wintermancer)'}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        print(None)
-        return
-
-    data = response.json()
-
-    if 'data' not in data:
-        print(None)
-        return
-
-    posts = data['data']['children']
-
-    for post in posts:
-        print(post['data']['title'])
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
