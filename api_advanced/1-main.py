0cb2fcef8fd07714258e8a38280f43f2d3ca4916
gusenga thierry <t.gusenga@alustudent.com>
2025-02-21 19:39:23 +0200

    Update 1-main.py

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,27 +1,12 @@
-import requests
+#!/usr/bin/python3
+"""
+Prints plz pass an argument.
+"""
+import sys
+from 1-top_ten import top_ten
 
-def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
-    params = {'limit': 10}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if response.status_code != 200:
-        print("OK")
-        return
-    
-    json_res = response.json()
-    
-    hot_articles = json_res.get('data', {}).get('children', [])
-    
-    if not hot_articles:
-        print("OK")
-        return
-    
-    for article in hot_articles:
-        print(article.get('data', {}).get('title', ''))
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
